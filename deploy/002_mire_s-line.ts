ead8aa19d01d7b794f73d2423d46cb608edb7831
Sylvain Laurent <s@6120.eu>
2022-04-19 21:18:36 +0200

    Deploying experience

--- deploy/002_mire_s-line.ts
+++ deploy/002_mire_s-line.ts
@@ -10,23 +10,33 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const NFTDescriptorLib = await ethers.getContract('NFTDescriptor', deployer);
   const EXP = await ethers.getContract('EXP', deployer);
 
+  console.log(EXP.address);
+  // if in test mode
+  // start impersonation
+  const impersonatedSigner = "0x6120932248DaFbDDb7e97279e10F9348b0E0242c";
+  //console.log(await hre.network.provider.request({
+  //  method: "hardhat_impersonateAccount",
+  //  params: [impersonatedSigner],
+  //}));
+
+  //const impersonatedDeployer = await ethers.getSigner(impersonatedSigner);
   await catchUnknownSigner(
     deploy('SLINE', {
       contract: 'MIRE',
-      from: deployer,
+      from: impersonatedSigner,
       log: true,
       libraries: {
         NFTDescriptor: NFTDescriptorLib.address,
       },
       proxy: {
-        owner: deployer,
+        owner: impersonatedSigner,
         execute: {
           init: {
             methodName: 'init',
             args: ['S-LINE', 'S†ine'],
           },
           onUpgrade: {
-            methodName: 'init',
+            methodName: 'migrate',
             args: [EXP.address],
           },
         },
@@ -34,5 +44,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     }),
     {log: true}
   );
+  // if in test mode
+  // stop impersonation
+//    await hre.network.provider.request({
+//  method: "hardhat_stopImpersonatingAccount",
+//  params: ["0x6120932248DaFbDDb7e97279e10F9348b0E0242c"],
+//});
 };
 export default func;
--- scripts/printAll.ts
+++ scripts/printAll.ts
@@ -9,6 +9,18 @@ function waitFor<T>(p: Promise<{wait: () => Promise<T>}>): Promise<T> {
   return p.then((tx) => tx.wait());
 }
 
+function dataURItoBlob(dataURI: string) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  return byteString;
+}
+
 async function main() {
   try {
     const log = debugModule('mire:script');
@@ -17,8 +29,8 @@ async function main() {
 
     const MIREContract = <MIRE>await ethers.getContract('SLINE', deployer);
     const WAITContract = <WAIT>await ethers.getContract('WAIT', deployer);
-    for (let i = 0; i < 10; i++) {
-      console.log(await MIREContract.tokenURI(i));
+    for (let i = 0; i < 13; i++) {
+      console.log(dataURItoBlob(await MIREContract.tokenURI(i)));
     }
   } catch (e) {
     console.log(e);
