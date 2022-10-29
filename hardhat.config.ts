3eb89834998bf96fce0e83aeef39fad098b81dd6
Sylvain Laurent <s@6120.eu>
2022-10-29 12:17:16 +0200

    Added latest version of Solidity to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -37,6 +37,16 @@ const config: HardhatUserConfig = {
           evmVersion: 'istanbul',
         },
       },
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          evmVersion: 'istanbul',
+        },
+      },
     ],
   },
   namedAccounts: {
